6a8942825118ba638a69e1b430513cef84f4c12b
Nasir <nasiralishigri786@gmail.com>
2024-12-23 13:14:19 +0500

    Updated hardhat file with setting

--- contracts/ProofOfExistence.sol
+++ contracts/ProofOfExistence.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.0;
 contract ProofOfExistence {
     mapping(bytes32 => uint256) public timestamps;
+
     function registerDocument(bytes32 documentHash) public {
         require(timestamps[documentHash] == 0, "Document already exists");
         timestamps[documentHash] = block.timestamp;
     }
+
     function getTimestamp(bytes32 documentHash) public view returns (uint256) {
         require(timestamps[documentHash] != 0, "Document not registered");
         return timestamps[documentHash];
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,98 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  // solidity: "0.8.24",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.27",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      // Local Hardhat network
+    },
+    ethereum: {
+      url: process.env.INFURA_PROJECT_ID, // Infura URL for Ethereum mainnet
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/", // BSC mainnet URL
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    bsc_test: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/", // BSC Testnet URL
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    kbc: {
+      url: "https://YOUR_KBC_CHAIN_RPC_URL", // Replace with actual KBC chain RPC URL
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    spoila_eth: {
+      url: process.env.SEPOILA_TESTNET_RPC, // Infura URL for Rinkeby testnet
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    spoila_base: {
+      url: process.env.SEPOILA_TESTNET_RPC, // Infura URL for Rinkeby testnet
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    shasta: {
+      url: "https://api.shasta.trongrid.io",
+      privateKey: process.env.PRIVATE_KEY,
+    },
+    mainnet: {
+      url: "https://api.trongrid.io",
+      privateKey: process.env.PRIVATE_KEY,
+    },
+    polygon_mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    fauji: {
+      url: `https://avalanche-fuji.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    amoy_testnet: {
+      url: `https://rpc-amoy.polygon.technology`, // Replace this with the actual URL for the Amoy testnet
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002, // Replace with the actual chain ID for Amoy testnet if different
+      gasPrice: 30000000000, // Gas price in wei (e.g., 50 gwei)
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETH_API_KEY,
+  },
+  // etherscan: {
+  //   apiKey: {
+  //     snowtrace: "snowtrace", // apiKey is not required, just set a placeholder
+  //   },
+  //   customChains: [
+  //     {
+  //       network: "snowtrace",
+  //       chainId: 43113,
+  //       urls: {
+  //         apiURL:
+  //           "https://api.routescan.io/v2/network/testnet/evm/43113/etherscan",
+  //         browserURL: "https://avalanche.testnet.localhost:8080",
+  //       },
+  //     },
+  //   ],
+  // },
 };
